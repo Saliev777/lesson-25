ea539bd40f96f66f697e1e549178f80fdbadd161
Zafar <143064723+zafar4ek@users.noreply.github.com>
2023-10-06 18:07:03 +0500

    Add files via upload

---
+++ 777.html/main.js
@@ -0,0 +1,156 @@
+// #1 Begin 1
+// var a = (prompt ("a tomonni kiriting"));
+// var b = (prompt (4));
+// const S = a * b ;
+//  alert (S);
+
+// Begin 2
+// var a = (prompt ("a tomonni kiriting"))
+// var b = (prompt ("b tomonni kirting"))
+// const S = a * b ;
+// alert (S);
+ 
+// Begin 3 
+// var a = (prompt ("a tomonni kiriitng"));
+// var b = (prompt ("b tomonni kiriting"));
+// const P =  2*a + 2*b;
+// alert (P);
+
+// begin 4 
+// let d = (prompt ("diametrni kiriting"));
+// let p = (prompt ("p ni kiriting")); 
+// const L = d * p ; 
+// alert (L); 
+
+// begin 5
+// let a = (prompt ("a ni kiriting")); 
+// const V = a * a * a; 
+// const S = 6 * a * a;
+// alert (V);
+// alert (S);
+ 
+// begin 6
+// let a = (prompt ('a ni kiriting'));
+// let b = (prompt ("b ni kiriting")); 
+// let c = (prompt ("c ni kiriting")); 
+// const V = a * b * c; 
+// const S = 2 * (a * b + b * c + a * c); 
+// alert (V);
+// alert (S);
+ 
+// begin 7
+// let r = (prompt ("r ni kiriting"));
+// let p = (prompt ("p ni kiriting"));
+// const L = 2 * p * r;
+// const S = p * r * r;
+// alert (L);
+// alert (S); 
+
+// begin 8
+// let a = (prompt ("a ni kiriting")); 
+// let b = (prompt ("b ni kiriting")); 
+// const Z = (a / 2 + b / 2);
+// alert (Z); 
+
+// begin 13 
+// let r_1 = (prompt ("r ni kiriting"));
+// let r_2 = (prompt ("r ni kiriting"));
+// let p = (prompt ("p ni kiriting")); 
+// const S_1 = p * r_1; 
+// const S_2 = p * r_2; 
+// const S_3 = p * (r_1 - r_2); 
+// alert (S_1); 
+// alert (S_2); 
+// alert (S_3); 
+
+// begin 32 
+// let t_f = (prompt ("gradusni kirititng"));
+// const t_c = (t_f - 32) * 5 / 9; 
+// alert (t_c);
+
+// integer 1
+// let L = (prompt ("santimetrni kiriting")); 
+// const M = L / 100; 
+// alert (M); 
+ 
+// integer 2 
+// let M = (prompt ("massani kiriting")); 
+// const T = M / 1000; 
+// alert (T); 
+
+// integer 3
+// let b = (prompt (" bayt hajmni kiriting")); 
+// const kb = b / 1024; 
+// alert (kb); 
+
+// integer 4 
+// let A = (prompt ("sonni kiriting")); 
+// let B = (prompt ("sonni kiriting")); 
+// if (A > B) {
+//     const Z = A/B;
+//     alert (Z)
+// } else{
+//     alert("incorrect");
+// }
+
+// integer 5 
+// let A = (prompt ("sonni kiriting")); 
+// let B = (prompt ("sonni kiriting"));
+// if (A > B) 
+// {
+//         const Z = A%B;
+//         alert (Z)
+// } else{
+//         alert("incorrect");
+// }
+
+// integer 6 
+// let A = (prompt ("on xonali sonni kiriting")); 
+// const Z = A / 10; 
+// const X = A / 1; 
+// alert (Z); 
+// alert (X); 
+
+// integer 7 
+// let A = (prompt ("2 xonali sonni kiriting")); 
+// const Z = A % 10 ; 
+// const X = A / 10 ; 
+// const C = Z + X ; 
+// alert (C);
+
+// integer 8 
+// let x = (prompt ("2 xonali sonni kiriting")); 
+// const a = x % 10;
+// const b = x / 10 - a / 10;  
+// alert (b);  
+
+// integer 19 
+// let N = (prompt ("sekundni kiriting")); 
+// const A = N / 60; 
+// alert (A); 
+
+// integer 20 
+// let N = (prompt ("sekundni kiriting")); 
+// const A = N / 3600; 
+// alert (A); 
+
+// integer 21 
+// let N = (prompt ("sekundni kiriting")); 
+// const A = N / 60; 
+// const B = N % 60; 
+// alert (A); 
+//  alert (B);
+
+// integer 22 
+// let N = (prompt ("sekundni kiriting")); 
+//  const A = N / 3600; 
+//  const B = N % 3600; 
+//  alert (A); 
+//  alert (B);
+
+// integer 23 
+// let N = (prompt ("sekundni kiriting")); 
+// const A = N / 3600; 
+// const B = N / 60; 
+// alert (A); 
+// alert (B); 
